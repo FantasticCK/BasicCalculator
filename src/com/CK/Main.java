ef083a97b0190be9cde3b361fa06ec58dc69c57f
CK <kainingc@usc.edu>
2019-07-23 17:26:23 -0700

    first commit

---
+++ src/com/CK/Main.java
@@ -0,0 +1,44 @@
+package com.CK;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = "(1+(4+5+2)-3)-(6+8)";
+//        String s = "2147483647";
+//        String s = "(1+4)";
+        Solution solution = new Solution();
+        System.out.println(solution.calculate(s));
+    }
+}
+
+class Solution {
+    public static int calculate(String s) {
+        int len = s.length(), sign = 1, result = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < len; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                int sum = s.charAt(i) - '0';
+                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                result += sum * sign;
+            } else if (s.charAt(i) == '+')
+                sign = 1;
+            else if (s.charAt(i) == '-')
+                sign = -1;
+            else if (s.charAt(i) == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+
+        }
+        return result;
+    }
+}
